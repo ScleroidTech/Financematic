706cb6b0387ba89330b26eca7cd04ab54e81ee52
Ganesh Kaple <gvklive@gmail.com>
2018-03-22 14:03:23 +0530

    Completed Dashboard View

--- app/src/main/java/com/scleroid/financematic/fragments/DashboardFragment.java
+++ app/src/main/java/com/scleroid/financematic/fragments/DashboardFragment.java
@@ -1,6 +1,10 @@
 package com.scleroid.financematic.fragments;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -11,7 +15,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,11 @@ import com.scleroid.financematic.utils.RecyclerTouchListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import timber.log.Timber;
+
 /**
  * Created by scleroid on 2/3/18.
  */
@@ -30,8 +38,20 @@ import java.util.List;
 
 public class DashboardFragment extends Fragment {
     TextView firstFragment;
+    @BindView(R.id.total_amount_text_view)
+    TextView totalAmountTextView;
+    @BindView(R.id.remaining_amount_text_view)
+    TextView remainingAmountTextView;
+    @BindView(R.id.lent_amount_text_view)
+    TextView lentAmountTextView;
+    @BindView(R.id.available_amount_text_view)
+    TextView availableAmountTextView;
+    @BindView(R.id.upcoming_events_text_view)
+    TextView upcomingEventsTextView;
+    @BindView(R.id.recycler_view_dashboard)
+    RecyclerView recyclerViewDashboard;
+    Unbinder unbinder;
     private List<Loan> loanList = new ArrayList<>();
-    private RecyclerView recyclerView;
     private LoanAdapter mAdapter;
 
     public DashboardFragment() {
@@ -51,23 +71,45 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ButterKnife.setDebug(true);
         // Inflate the layout for this fragment
-        LinearLayout wrapper = new LinearLayout(getActivity());
-        View rootView = inflater.inflate(R.layout.dashboard, wrapper, true);
+        View rootView = inflater.inflate(R.layout.dashboard, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
 
 
 //Intend
         firstFragment = rootView.findViewById(R.id.total_amount_text_view);
         firstFragment.setOnClickListener(v -> loadFragment(new Fragment_passbook()));
 
-        recyclerView = rootView.findViewById(R.id.recycler_view_dashboard);
+        // recyclerView = rootView.findViewById(R.id.recycler_view_dashboard);
 
+        prepareLoanData();
+
+
+        setupRecyclerView();
+        textViewExperiments(upcomingEventsTextView);
+        textViewExperiments(totalAmountTextView);
+
+        return rootView;
+    }
+
+    private void textViewExperiments(TextView textView) {
+        Shader textShaderOther = new LinearGradient(0, 0, 300, textView.getLineHeight(),
+                new int[]{0xFF852D91, 0xFF6253E1, 0xFF00E0E4},
+                null, Shader.TileMode.CLAMP);
+        Timber.i("textview Values" + textView.getTextScaleX() + " " + textView.getTextSize() + " " + textView.getMeasuredWidth() + " " + textView.getLineHeight());
+        Shader textShader = new LinearGradient(0, 0, 150, textView.getLineHeight(),
+                new int[]{Color.GRAY, Color.BLUE},
+                null, Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(textShaderOther);
+    }
+
+    private void setupRecyclerView() {
         mAdapter = new LoanAdapter(loanList);
 
-        recyclerView.setHasFixedSize(true);
-
+        recyclerViewDashboard.setHasFixedSize(true);
 
 
         // vertical RecyclerView
@@ -78,19 +120,19 @@ public class DashboardFragment extends Fragment {
         // keep movie_list_row.xml width to `wrap_content`
         // RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
 
-        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerViewDashboard.setLayoutManager(mLayoutManager);
 
 
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewDashboard.setItemAnimator(new DefaultItemAnimator());
 
-        recyclerView.setAdapter(mAdapter);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+        recyclerViewDashboard.setAdapter(mAdapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerViewDashboard.getContext(),
                 DividerItemDecoration.VERTICAL);
-        // recyclerView.addItemDecoration(dividerItemDecoration);
+        //  recyclerView.addItemDecoration(dividerItemDecoration);
 
 
         // row click listener
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity().getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+        recyclerViewDashboard.addOnItemTouchListener(new RecyclerTouchListener(getActivity().getApplicationContext(), recyclerViewDashboard, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
                 Loan loan = loanList.get(position);
@@ -102,30 +144,22 @@ public class DashboardFragment extends Fragment {
 
             }
         }));
-
-        prepareLoanData();
-
-        return wrapper;
     }
 
 
     private void prepareLoanData() {
         Loan loan = new Loan("Shahrukh Khan", "2000", "2 Days to go");
-       loanList.add(loan);
+        loanList.add(loan);
         loan = new Loan("Akshay Kumar", "1000", "3 days to go");
         loanList.add(loan);
         loan = new Loan("Amitabh Bachchan", "1500", "5 days to go");
         loanList.add(loan);
 
 
-
         // notify adapter about data set changes
         // so that it will render the list with new data
-        mAdapter.notifyDataSetChanged();
+        //mAdapter.notifyDataSetChanged();
     }
-
-
-
 
 
     //for intend passook
@@ -140,4 +174,9 @@ public class DashboardFragment extends Fragment {
     }
 
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
