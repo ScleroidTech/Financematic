7217b7a47e380d97f8b98d4b18637db54d0795b8
gvklive <gvklive@gmail.com>
2018-03-27 13:14:49 +0530

    ExpenseFragment

---
+++ app/src/main/java/com/scleroid/financematic/adapter/ExpenseAdapter.java
@@ -0,0 +1,8 @@
+package com.scleroid.financematic.adapter;
+
+/**
+ * Created by scleroid on 27/3/18.
+ */
+
+public class ExpenseAdapter {
+}
--- app/src/main/java/com/scleroid/financematic/fragments/ExpenseFragment.java
+++ app/src/main/java/com/scleroid/financematic/fragments/ExpenseFragment.java
@@ -4,10 +4,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -17,6 +21,11 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.scleroid.financematic.R;
+import com.scleroid.financematic.adapter.ExpenseAdapter;
+import com.scleroid.financematic.adapter.PassbookAdapter;
+import com.scleroid.financematic.model.Passbook;
+import com.scleroid.financematic.model.expense;
+import com.scleroid.financematic.utils.RecyclerTouchListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +36,9 @@ import butterknife.Unbinder;
 
 
 public class ExpenseFragment extends Fragment {
+    private List<expense> passbookList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ExpenseAdapter mAdapter;
 
     @BindView(R.id.pie_chart_expense)
     PieChart mChart;
@@ -58,6 +70,49 @@ public class ExpenseFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_expense, container, false);
+        //recyler add
+        recyclerView = rootView.findViewById(R.id.expense_recycler);
+
+        mAdapter = new PassbookAdapter(passbookList);
+
+        recyclerView.setHasFixedSize(true);
+
+       /* recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this.getContext()));*/
+
+        // vertical RecyclerView
+        // keep movie_list_row.xml width to `match_parent`
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+
+        // horizontal RecyclerView
+        // keep movie_list_row.xml width to `wrap_content`
+        // RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerView.setAdapter(mAdapter);
+
+        // row click listener
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity().getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Passbook passbook = passbookList.get(position);
+                Toast.makeText(getActivity().getApplicationContext(), passbook.getPassbook_received_money() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+
+
+
+
+
         unbinder = ButterKnife.bind(this, view);
         //      float[] data = {450, 630, 300, 200, 400};
         //    mChart.setData(data);
@@ -126,4 +181,37 @@ public class ExpenseFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
+
+    private void prepareLoanData() {
+        Passbook passbook = new Passbook("14/6/2018 ", "Person Name 1", "50,000","");
+        passbookList.add(passbook);
+        passbook = new Passbook("12/6/2018 ", "Person Name 2", "","3000");
+        passbookList.add(passbook);
+        passbook = new Passbook("12/6/2018 ", "Person Name 3", "","4000");
+        passbookList.add(passbook);
+        passbook = new Passbook("12/6/2018 ", "Person Name 1", "1200","");
+        passbookList.add(passbook);
+        passbook = new Passbook("1/2/2018 ", "Person Name 2", "4000","");
+        passbookList.add(passbook);
+
+        passbook = new Passbook("10/1/2018 ", "Person Name 3", "","2000");
+        passbookList.add(passbook);
+
+        passbook = new Passbook("12/1/2018 ", "Person Name 1", "4000","");
+        passbookList.add(passbook);
+
+
+
+
+
+        // notify adapter about data set changes
+        // so that it will render the list with new data
+        mAdapter.notifyDataSetChanged();
+    }
+
+
+
+
+
 }
