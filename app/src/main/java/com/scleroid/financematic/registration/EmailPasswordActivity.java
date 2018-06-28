70fa0e2f669e76fb8ccb7c810c445cbf2d11973d
Ganesh Kaple <gvklive@gmail.com>
2018-06-28 17:35:45 +0530

    Deleted the Activities which weren't needed

--- app/src/main/java/com/scleroid/financematic/registration/EmailPasswordActivity.java
+++ app/src/main/java/com/scleroid/financematic/registration/EmailPasswordActivity.java
@@ -178,29 +178,26 @@ public class EmailPasswordActivity extends BaseActivity implements
 
 		// [START sign_in_with_email]
 		mAuth.signInWithEmailAndPassword(email, password)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						if (task.isSuccessful()) {
-							// Sign in success, update UI with the signed-in user's information
-							Log.d(TAG, "signInWithEmail:success");
-							FirebaseUser user = mAuth.getCurrentUser();
-							updateUI(user);
-						} else {
-							// If sign in fails, display a message to the user.
-							Log.w(TAG, "signInWithEmail:failure", task.getException());
-							Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-									Toast.LENGTH_SHORT).show();
-							updateUI(null);
-						}
-
-						// [START_EXCLUDE]
-						if (!task.isSuccessful()) {
-							mStatusTextView.setText(R.string.auth_failed);
-						}
-						hideProgressDialog();
-						// [END_EXCLUDE]
+				.addOnCompleteListener(this, task -> {
+					if (task.isSuccessful()) {
+						// Sign in success, update UI with the signed-in user's information
+						Log.d(TAG, "signInWithEmail:success");
+						FirebaseUser user = mAuth.getCurrentUser();
+						updateUI(user);
+					} else {
+						// If sign in fails, display a message to the user.
+						Log.w(TAG, "signInWithEmail:failure", task.getException());
+						Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+								Toast.LENGTH_SHORT).show();
+						updateUI(null);
 					}
+
+					// [START_EXCLUDE]
+					if (!task.isSuccessful()) {
+						mStatusTextView.setText(R.string.auth_failed);
+					}
+					hideProgressDialog();
+					// [END_EXCLUDE]
 				});
 		// [END sign_in_with_email]
 	}
