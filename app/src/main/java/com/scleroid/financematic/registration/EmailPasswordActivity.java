03783a35ebdf8258d8684590fef1efb1619f3c99
Ganesh Kaple <gvklive@gmail.com>
2018-06-29 15:18:40 +0530

    Changed App name

--- app/src/main/java/com/scleroid/financematic/registration/EmailPasswordActivity.java
+++ app/src/main/java/com/scleroid/financematic/registration/EmailPasswordActivity.java
@@ -26,7 +26,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.scleroid.financematic.MainActivity;
@@ -149,26 +148,24 @@ public class EmailPasswordActivity extends BaseActivity implements
 
 		// [START create_user_with_email]
 		mAuth.createUserWithEmailAndPassword(email, password)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						if (task.isSuccessful()) {
-							// Sign in success, update UI with the signed-in user's information
-							Log.d(TAG, "createUserWithEmail:success");
-							FirebaseUser user = mAuth.getCurrentUser();
-							updateUI(user);
-						} else {
-							// If sign in fails, display a message to the user.
-							Log.w(TAG, "createUserWithEmail:failure", task.getException());
-							Toast.makeText(EmailPasswordActivity.this, "Authentication Fail or wrong id",
-									Toast.LENGTH_SHORT).show();
-							updateUI(null);
-						}
-
-						// [START_EXCLUDE]
-						hideProgressDialog();
-						// [END_EXCLUDE].class
+				.addOnCompleteListener(this, task -> {
+					if (task.isSuccessful()) {
+						// Sign in success, update UI with the signed-in user's information
+						Log.d(TAG, "createUserWithEmail:success");
+						FirebaseUser user = mAuth.getCurrentUser();
+						updateUI(user);
+					} else {
+						// If sign in fails, display a message to the user.
+						Log.w(TAG, "createUserWithEmail:failure", task.getException());
+						Toast.makeText(EmailPasswordActivity.this,
+								"Authentication Fail or wrong id",
+								Toast.LENGTH_SHORT).show();
+						updateUI(null);
 					}
+
+					// [START_EXCLUDE]
+					hideProgressDialog();
+					// [END_EXCLUDE].class
 				});
 		// [END create_user_with_email]
 	}
