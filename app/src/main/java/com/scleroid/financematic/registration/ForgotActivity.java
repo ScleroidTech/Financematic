26712dfcacffe4aeee82078fbe105ce57a1f441d
scleroid <sontakke.kumar100@gmail.com>
2018-06-29 12:33:07 +0530

    Add forgot password,email

--- app/src/main/java/com/scleroid/financematic/registration/ForgotActivity.java
+++ app/src/main/java/com/scleroid/financematic/registration/ForgotActivity.java
@@ -2,12 +2,13 @@ package com.scleroid.financematic.registration;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.scleroid.financematic.MainActivity;
 import com.scleroid.financematic.R;
 import com.scleroid.financematic.base.BaseActivity;
 
-public class ForgotActivity extends MainActivity {
+public class ForgotActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
